71e0677cce93fa2a4e41c4e7e223cd979fad9b6d
Karina Skirmante <karina.krinkele@venta.lv>
2022-05-05 15:26:45 +0300

    Edit: add data jpa nnotation to Professor

--- src/main/java/lv/venta/demo/model/Professor.java
+++ src/main/java/lv/venta/demo/model/Professor.java
@@ -1,5 +1,12 @@
 package lv.venta.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,16 +18,24 @@ import lombok.ToString;
 @Setter
 @ToString
 @NoArgsConstructor
+@Table
+@Entity
 public class Professor {
 
 	//1.mainigie
+	@Column(name = "IdPr")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Setter(value=AccessLevel.NONE)
 	private int idPr;
 	
+	@Column(name = "Name")
 	private String name;
 	
+	@Column(name = "Surname")
 	private String surname;
 	
+	@Column(name = "Degree")
 	private ProfDegree degree;
 
 	
