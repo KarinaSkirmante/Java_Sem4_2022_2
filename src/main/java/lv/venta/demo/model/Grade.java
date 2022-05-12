f5ec08035150dcc3d0305187a6ddc48367252415
Karina Skirmante <karina.krinkele@venta.lv>
2022-05-12 13:26:42 +0300

    Add: Grade and Students

---
+++ src/main/java/lv/venta/demo/model/Grade.java
@@ -0,0 +1,42 @@
+package lv.venta.demo.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Table
+@Entity
+public class Grade {
+
+        @Column(name = "IdGr")
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Setter(value=AccessLevel.NONE)
+        private int idGr;
+        
+        @Min(0)
+        @Max(10)
+        @Column(name = "Value")
+        private int value;
+        
+        private Student student;
+        
+        private Course course;
+}
+
